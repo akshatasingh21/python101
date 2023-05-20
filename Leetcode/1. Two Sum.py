dd88fd9fa6d70e2f6bc8b5a5fb3f98453adfd2b8
akshatasingh21 <128216318+akshatasingh21@users.noreply.github.com>
2023-05-20 10:51:46 +0000

    Two sum code

---
+++ Leetcode/1. Two Sum.py
@@ -0,0 +1,29 @@
+# O(n) Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m = {}
+
+        for i in range(len(nums)):
+            if target-nums[i] in m:
+                return [m[target-nums[i]], i]
+            m[nums[i]] = i
+
+
+
+# O(n^2) Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        flag = 1
+        for i in range(len(nums)-1):
+            if flag == 0:
+                break
+
+            x = nums[i]
+
+            for j in range(i+1, len(nums)):
+                z = nums[j]
+                if x+z == target:
+                    flag = 0
+                    list = [i,j]
+                    break
+        return list
